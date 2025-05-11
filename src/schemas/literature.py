5c5bb61f8c9b41acaf637184847b995550048ada
aepatino5112 <aepatino5112@gmail.com>
2025-05-11 14:31:10 -0400

    Add Literature Schemas

---
+++ src/schemas/literature.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, date
+
+class Literature(BaseModel):
+    id_literature: int
+    id_river: int
+    id_multimedia: int
+    created_at: datetime
+    title: str
+    author: str
+    publication_year: Optional[date]
+    summary: str
+
+class InsertLiterature(BaseModel):
+    id_river: int
+    id_multimedia: int
+    created_at: datetime
+    title: str
+    author: str
+    publication_year: Optional[date]
+    summary: str
+
+class UpdateLiterature(BaseModel):
+    id_literature: int
+    id_river: int
+    id_multimedia: int
+    created_at: Optional[datetime]
+    title: Optional[str]
+    author: Optional[str]
+    publication_year: Optional[str]
+    summary: Optional[str]
+
+class DeleteLiterature(BaseModel):
+    id_literature: int
