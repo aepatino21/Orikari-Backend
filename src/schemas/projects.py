c2b1246f6688c30ce4b7fc0e88efd5e17dd3bb4a
aepatino5112 <aepatino5112@gmail.com>
2025-05-11 14:56:02 -0400

    Add Projects Schemas

---
+++ src/schemas/projects.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class Projects(BaseModel):
+    id_project: int
+    id_user: int
+    id_user_multimedia: int
+    created_at: datetime
+    title: str
+    project: str
+    category: str
+
+class InsertProjects(BaseModel):
+    id_user: int
+    id_user_multimedia: int
+    created_at: datetime
+    title: str
+    project: str
+    category: str
+
+class UpdateProjects(BaseModel):
+    id_project: int
+    id_user: int
+    id_user_multimedia: int
+    created_at: Optional[datetime]
+    title: Optional[str]
+    project: Optional[str]
+    category: Optional[str]
+
+class DeleteProjects(BaseModel):
+    id_project: int
+    id_user: int
+    id_user_multimedia: int
