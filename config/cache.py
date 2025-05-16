da8269de26b2f7d89ce29660ead64474dd6db51f
aepatino5112 <aepatino5112@gmail.com>
2025-05-15 23:03:32 -0400

    Add Redis Cloud support

--- config/cache.py
+++ config/cache.py
@@ -1,4 +1,17 @@
+import os
 from redis import Redis
+from dotenv import load_dotenv
 
-# Inicializa la conexcion a Redis (Valkey actua como un servidor Redis)
-cache = Redis(host='localhost', port=6379, db=0, decode_responses=True)
+load_dotenv()
+
+host = os.getenv("REDIS_HOST")
+port = int(os.getenv("REDIS_PORT"))
+password = os.getenv("REDIS_PASSWORD")
+
+# Inicializa la conexion a Redis (Valkey actua como un servidor Redis)
+cache = Redis(
+    host=host,
+    port=port,
+    password=password,
+    decode_responses=True)
+
