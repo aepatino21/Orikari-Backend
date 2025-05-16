525858b4df6b19e6b6325b8835a51e1422a58ee3
aepatino5112 <aepatino5112@gmail.com>
2025-05-15 22:20:25 -0400

    Undeprecated: Rivers is active again with cache

--- endpoints/river_endpoints.py
+++ endpoints/river_endpoints.py
@@ -1,74 +1,67 @@
 from config.supabase_config import supabase
+from config.cache import cache
 from fastapi import APIRouter, HTTPException
 from schemas.river import River, DeleteRiver, InsertRiver, UpdateRiver
-from typing import List
-from endpoints.statistics_endpoints import get_statistics
 from endpoints.articles_endpoints import get_latest_articles
 from endpoints.industry_endpoints import get_industry_images
 from endpoints.society_endpoints import get_society
 from endpoints.literature_endpoints import get_latest_literature
 from endpoints.collage_endpoints import get_latest_collage
 from endpoints.zoo_endpoints import get_zoo_hero
+import json
 
 # Instancia del router.
 router = APIRouter(prefix="/river", tags = ["River"])
 
-# !! DEPRECATED
-# # Get all rivers.
-# @router.get('/{id}')
-# async def get_rivers(id: int):
-#     try:
+# Get all rivers.
+@router.get('/{id}')
+async def get_rivers(id: int):
+    key = f'river_{id}'
+    try:
 
-#         response = (
-#             supabase.table("River")
-#             .select("name, created_at")
-#             .eq("id", id)
-#             .execute()
-#         )
+        cached_data = cache.get(key)
 
-#         data = response.data[0]
-#         river_id = id
-#         rivers = {}
+        if cached_data:
+            return json.loads(cached_data)
 
-#         # Get statistics data
-#         statistics = await get_statistics(river_id)
+        river_id = id
+        rivers = {}
 
-#         # Get latest articles data
-#         latest_articles = await get_latest_articles(river_id)
+        # Get latest articles data
+        latest_articles = await get_latest_articles(river_id)
 
-#         # Get industry images
-#         industry_images = await get_industry_images(river_id)
+        # Get industry images
+        industry_images = await get_industry_images(river_id)
 
-#         # Get Water & Society images
-#         society_images = await get_society()
+        # Get Water & Society images
+        society_images = await get_society()
 
-#         # Get the latest 6 books in library
-#         latest_literature = await get_latest_literature(river_id)
+        # Get the latest 6 books in library
+        latest_literature = await get_latest_literature(river_id)
 
-#         # Get the latest 6 images in collage
-#         latest_collage = await get_latest_collage(river_id)
+        # Get the latest 6 images in collage
+        latest_collage = await get_latest_collage(river_id)
 
-#         # Get Zoo hero
-#         zoo_hero = await get_zoo_hero()
+        # Get Zoo hero
+        zoo_hero = await get_zoo_hero()
 
-#         # Assemble the river JSON
-#         rivers.update({
-#             "id": river_id,
-#             "name": data.get('name'),
-#             "created_at": data.get('created_at'),
-#             "statistics": statistics,
-#             "latest_articles": latest_articles,
-#             "industry_images": industry_images,
-#             "society_images": society_images,
-#             "zoo_hero": zoo_hero,
-#             "latest_literature": latest_literature,
-#             "latest_collage": latest_collage
-#         })
+        # Assemble the river JSON
+        rivers.update({
+            "id": river_id,
+            "latest_articles": latest_articles,
+            "industry_images": industry_images,
+            "society_images": society_images,
+            "zoo_hero": zoo_hero,
+            "latest_literature": latest_literature,
+            "latest_collage": latest_collage
+        })
 
-#         return rivers
+        cache.setex(key, 1200, json.dumps(rivers))
 
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+        return rivers
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
  # Post a river
