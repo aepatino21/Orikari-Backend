d22c12b6b3601ccada1934748677929b17d85ed7
aepatino5112 <aepatino5112@gmail.com>
2025-05-15 21:59:57 -0400

    Add cache to Collage endpoints

--- endpoints/collage_endpoints.py
+++ endpoints/collage_endpoints.py
@@ -1,7 +1,9 @@
 from config.supabase_config import supabase
+from config.cache import cache
 from schemas.multimedia import Multimedia, InsertMultimedia, UpdateMultimedia, DeleteMultimedia
 from endpoints.multimedia_endpoints import get_multimedia
 from fastapi import APIRouter, HTTPException
+import json
 
 # Instancia del router
 router = APIRouter(prefix='/collage', tags=['Collage'])
@@ -34,7 +36,13 @@ async def get_latest_collage(id_river: int):
 # Get all collage images
 @router.get('/{id_river}')
 async def get_collage(id_river: int):
+    key = f'collage_{id_river}'
     try:
+
+        cached_data = cache.get(key)
+
+        if cached_data:
+            return json.loads(cached_data)
 
         if id_river == 1:
             river = 'orinoco'
@@ -62,6 +70,8 @@ async def get_collage(id_river: int):
             'collage_hero': collage_hero.data[0],
             'collage': data
         })
+
+        cache.setex(key, 600, json.dumps(collage))
 
         return collage
 
